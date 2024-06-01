73af866fb737b119b195ea4d42c28182a8e7b09d
ShinYou-bin <sinddol07@naver.com>
2024-06-02 03:06:33 +0900

    DB 생성 1

---
+++ app/src/main/java/com/example/timeconversionapplication/MyAdapter.kt
@@ -0,0 +1,4 @@
+package com.example.timeconversionapplication
+
+class MyAdapter {
+}
---
+++ app/src/main/java/com/example/timeconversionapplication/Product.kt
@@ -0,0 +1,5 @@
+package com.example.timeconversionapplication
+
+class Product (val product_name: String,
+               val price: Int,
+               val memo: String)
