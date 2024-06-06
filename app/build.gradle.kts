60bf558e8c50975059b31fc600dc0ac8165cd159
RohJongWook <whddnr0103490@naver.com>
2024-06-06 22:45:56 +0900

    salaryday에 db에서 가져온 월급일 집어넣을 수 있게 만들었음

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,7 +39,7 @@ android {
 }
 
 dependencies {
-
+    implementation("com.github.prolificinteractive:material-calendarview:2.0.1")
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.12.0")
--- app/src/main/java/com/example/timeconversionapplication/CalendarFragment.kt
+++ app/src/main/java/com/example/timeconversionapplication/CalendarFragment.kt
@@ -1,18 +1,33 @@
 package com.example.timeconversionapplication
 
+import android.app.Activity
 import android.content.ContentValues
+import android.content.Context
 import android.content.Intent
 import android.database.sqlite.SQLiteConstraintException
+import android.graphics.Color
+import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.text.style.ForegroundColorSpan
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.timeconversionapplication.databinding.FragmentCalendarBinding
-
+import com.prolificinteractive.materialcalendarview.CalendarDay
+import com.prolificinteractive.materialcalendarview.DayViewDecorator
+import com.prolificinteractive.materialcalendarview.DayViewFacade
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView
+import com.prolificinteractive.materialcalendarview.spans.DotSpan
+import java.time.LocalDate
+import java.time.ZoneId
+import java.util.Calendar
+import java.util.Date
+import java.time.DayOfWeek
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 //private const val ARG_PARAM1 = "param1"
@@ -23,7 +38,9 @@ import com.example.timeconversionapplication.databinding.FragmentCalendarBinding
  * Use the [CalendarFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
+
 class CalendarFragment : Fragment() {
+
     lateinit var binding: FragmentCalendarBinding
     private lateinit var dbHelper: MyDatabase.MyDBHelper
     private lateinit var adapter: CalendarAdapter
@@ -44,9 +61,9 @@ class CalendarFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentCalendarBinding.inflate(inflater, container, false)
-
+        val calendarList = mutableListOf<CalendarDay>()
         // 날짜가 변경될 때 day 값 업데이트 및 DB에서 데이터 조회
-        var day = ""
+        /*var day = ""
         binding.calenderView.setOnDateChangeListener { calendarView, year, month, dayOfMonth ->
             binding.month.text = (month+1).toString()
             binding.day.text = dayOfMonth.toString()
@@ -57,8 +74,7 @@ class CalendarFragment : Fragment() {
             if (workTime != null) {
                 binding.wage.text = workTime.wage.toString()
             }
-        }
-
+        }*/
         // RecyclerView 설정 - recyclerView1은  일급, recyclerView2 는 디데이
         binding.recyclerView1.layoutManager = LinearLayoutManager(requireContext())
         binding.recyclerView1.addItemDecoration(DividerItemDecoration(requireContext(), LinearLayoutManager.VERTICAL))
@@ -69,15 +85,69 @@ class CalendarFragment : Fragment() {
         val salaryList = dbHelper.selectAll(MyDatabase.MyDBContract.WorkTime.TABLE_NAME, WorkTime::class.java)
         // 데이터베이스에서 Dday 데이터 가져오기
         val ddayList = dbHelper.selectAll(MyDatabase.MyDBContract.Dday.TABLE_NAME, Dday::class.java)
-
         // 어댑터 설정 및 RecyclerView에 어댑터 할당
         adapter = CalendarAdapter(salaryList as MutableList<Any>)
         binding.recyclerView1.adapter = adapter
         adapter2 = CalendarAdapter(ddayList as MutableList<Any>)
         binding.recyclerView2.adapter = adapter2
 
+
+        val toDayDeco = context?.let { ToDayDecorator(it) }
+
+
+        val currentYear = Calendar.getInstance().get(Calendar.YEAR)
+
+
+
+
+        val salaryDay = 15 //여기에 db에서 가져온 월급일 입력 15는 예시
+        for (month in 1..12) {
+            calendarList.add(CalendarDay.from(currentYear, month, salaryDay))
+        }
+
+
+
+
+        val eventDeco = EventDecorator(calendarList, Activity(), Color.BLUE)
+        binding.calendarView.addDecorators(toDayDeco, eventDeco)
+
         return binding.root
     }
+    inner class ToDayDecorator(context: Context) : DayViewDecorator {
+
+        private var date = CalendarDay.today()
+        private val drawable = ContextCompat.getDrawable(context, R.drawable.button_round)
+
+        override fun shouldDecorate(day: CalendarDay?): Boolean {
+            return day?.equals(date)!!
+        }
+
+        override fun decorate(view: DayViewFacade?) {
+            if (drawable != null) {
+                view?.setBackgroundDrawable(drawable)
+            }
+        }
+
+    }
+
+    inner class EventDecorator(dates: Collection<CalendarDay>?, context: Activity, color: Int) : DayViewDecorator {
+        private var dates: HashSet<CalendarDay>
+        private var color = 0
+
+        init {
+            this.dates = dates?.let { HashSet(it) } ?: HashSet()
+            this.color = color
+        }
+
+        override fun shouldDecorate(day: CalendarDay?): Boolean {
+            return dates.contains(day)
+        }
+
+        override fun decorate(view: DayViewFacade?) {
+            view?.addSpan(DotSpan(5F, color))
+        }
+    }
+
 
     companion object {
         /**
